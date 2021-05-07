612af66747fdabf316fdaba04996efd2bdbea634
Creampanda <andrewfromtokyo@gmail.com>
2021-05-07 20:39:51 +0300

    done 2nd lab

--- lab2/tb/counter_tb.v
+++ lab2/tb/counter_tb.v
@@ -21,74 +21,56 @@
 
 
 module counter_tb(
-
     );
 
   reg          clk100_i;
   reg          rstn_i;
-  reg   [11:0] sw_i;
-  reg   [2:0]  key_i;
+  reg   [9:0] sw_i;
+  reg   [1:0]  key_i;
   wire  [9:0]  ledr_o;
   wire  [6:0]  hex1_o;
   wire  [6:0]  hex0_o;
 
-    counter DUT (
-      .clk100_i  ( clk100_i ),
-      .rstn_i    ( rstn_i   ),
-      .sw_i      ( sw_i     ),
-      .key_i     ( key_i    ),
-      .ledr_o    ( ledr_o   ),
-      .hex1_o    ( hex1_o   ),
-      .hex0_o    ( hex0_o   )
-    );
-
-    initial begin
-    
-      sw_i[7:0] = 8'b10101010;
-      sw_i[11:7] = 3'b000;
-      key_i[2:0] = 3'b111;
-      #55
-      key_i[1] = 1'b0;
-      #40
-      key_i[1] = 1'b1;
-      #40
-      sw_i[7:0] = 8'b00001111;
-      #40
-      key_i[0] = 1'b0;
-      #40
-      key_i[0] = 1'b1;
-      #40
-      key_i[0] = 1'b0;
-      #40
-      key_i[0] = 1'b1;
-      #40
-      sw_i[7:0] = 8'b00101011;
-      #40
-      sw_i[11] = 1'b1;
-      #40
-      key_i[0] = 1'b0;
-      #40
-      key_i[0] = 1'b1;
-      #40
-      key_i[2] = 1'b0;
-      #40
-      key_i[2] = 1'b1;
-      #40
-      sw_i[7:0] = 8'b00001111;
-      #40
-      key_i[0] = 1'b0;
-      #40
-      key_i[0] = 1'b1;
-      
-      
-      
-    end
-
-
-    always begin 
-      clk100_i = 1'b1;
-      #10;
-      clk100_i = 1'b0;
-      #10;
-    end
+  counter DUT (
+    .clk100_i  ( clk100_i ),
+    .rstn_i    ( rstn_i   ),
+    .sw_i      ( sw_i     ),
+    .key_i     ( key_i    ),
+    .ledr_o    ( ledr_o   ),
+    .hex1_o    ( hex1_o   ),
+    .hex0_o    ( hex0_o   )
+  );
+  initial begin
+    key_i = 2'b11;
+    sw_i = 10'b00000_00000;
+    key_i[1] = 1'b0;
+    #10
+    key_i[1] = 1'b1;
+    #50
+    key_i[0] = 1'b0;
+    #10
+    key_i[0] = 1'b1;
+    #50
+    sw_i = 10'b00000_00001;
+    #20
+    key_i[0] = 1'b0;
+    #10
+    key_i[0] = 1'b1;
+    #50
+    sw_i = 10'b00000_00011;
+    #20
+    key_i[0] = 1'b0;
+    #10
+    key_i[0] = 1'b1;
+    #50
+    key_i[1] = 1'b0;
+    #10
+    key_i[1] = 1'b1;
+  end
+  always begin 
+    clk100_i = 1'b1;
+    #10;
+    clk100_i = 1'b0;
+    #10;
+  end
 endmodule
